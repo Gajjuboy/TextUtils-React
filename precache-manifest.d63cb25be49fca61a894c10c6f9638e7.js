c8f1fa0899f5a9f9d226d506bcd91a00869bc7fd
pushpraj <pushpraj@startappss.com>
2023-09-01 19:19:42 +0530

    Updates

---
+++ precache-manifest.d63cb25be49fca61a894c10c6f9638e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf94c19796db9c0e3273",
+    "url": "/TextUtils-React/static/css/main.fed9c6fb.chunk.css"
+  },
+  {
+    "revision": "bf94c19796db9c0e3273",
+    "url": "/TextUtils-React/static/js/main.60dd92cc.chunk.js"
+  },
+  {
+    "revision": "a8ffbc9c2d7a749a4b7e",
+    "url": "/TextUtils-React/static/js/runtime~main.487181dd.js"
+  },
+  {
+    "revision": "5fcf4af6b98fbb883785",
+    "url": "/TextUtils-React/static/js/2.9ac2001b.chunk.js"
+  },
+  {
+    "revision": "17568128810c26d53c27",
+    "url": "/TextUtils-React/static/js/3.5e86fb12.chunk.js"
+  },
+  {
+    "revision": "0dfd509a13dbe5c6ee8f1fe01ca54309",
+    "url": "/TextUtils-React/index.html"
+  }
+];
